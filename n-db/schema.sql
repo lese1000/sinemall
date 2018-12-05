3ea8deb243de4c0fee931c7e511537825967d2c9
lese1000 <598936703@qq.com>
2018-12-05 15:38:15 +0800

    remove n-db

--- n-db/schema.sql
+++
@@ -1,6 +0,0 @@
-drop database if exists sanmall;
-drop user sanmall@'%';
-create database sanmall default character set utf8mb4 collate utf8mb4_bin;
-create user 'sanmall'@'%' identified by 'sm1101@!';
-grant all privileges on sanmall.* to 'sanmall'@'%';
-flush privileges;
